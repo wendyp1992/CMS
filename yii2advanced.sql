f5128ffef87e0b5a4c59a201f1e1962270fb22ce
Wendy Estupiñán <wendy.paola.estupinan@gmail.com>
2016-11-28 14:58:37 -0500

    CMS_Terminado_WEB_SERVICES

--- yii2advanced.sql
+++ yii2advanced.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-11-2016 a las 05:45:41
+-- Tiempo de generación: 28-11-2016 a las 20:52:11
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 5.5.38
 
